f19a41144ef5d60d355c8d3ca23b6e1d8015b4e5
Sashkoe <revolver.bmx@gmail.com>
2024-08-11 19:36:18 +0200

    1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import 'modern-normalize';
+import './App.css';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactForm from './components/ContactForm/ContactForm';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from './redux/contactsOps';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch])
+  return (
+    <div className='box'>
+      <h1>Phone book</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
